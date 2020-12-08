ca995c29389814e1901f9dcf576dfc4c452fb6fe
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-12-08 11:36:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 const batteries = () => {
   
-  let totalBAtteries = batteryBatches.reduce((acc, val) => {
+  let totalBatteries = batteryBatches.reduce((acc, val) => {
     return acc + val
   })
-  
+  return totalBatteries
 };
